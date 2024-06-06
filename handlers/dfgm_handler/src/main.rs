c29d18b7896df26ec41e51bed881c1798e20b714
Devin <headrick@ualberta.ca>
2024-06-06 14:00:39 -0600

    wrote handler readme

---
+++ handlers/dfgm_handler/src/main.rs
@@ -0,0 +1,18 @@
+/*
+Written By Devin Headrick
+Summer 2024
+
+All handlers:   
+    - handle all communication with their respective subsystem via interface 
+    - communicate with other FSW via IPC 
+    - take opcodes from messages passed to them, and execute their related functionality
+*/
+
+
+// Trait for all handlers
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
