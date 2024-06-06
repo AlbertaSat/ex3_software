e324f2d92edd33620ee3233dc8e76fc56891ed24
rrasmuss07 <rrasmuss@ualberta.ca>
2024-06-06 10:15:14 -0600

    Got rid of default tests

--- libs/message_structure/src/lib.rs
+++ libs/message_structure/src/lib.rs
@@ -4,11 +4,5 @@ pub fn presence() {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    
 }
