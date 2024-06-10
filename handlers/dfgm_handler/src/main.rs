7b84f36f8b9e3484f90f72fe6c68e28343933b09
Devin <headrick@ualberta.ca>
2024-06-10 13:59:04 -0600

    print beginning dfgm handler now

--- handlers/dfgm_handler/src/main.rs
+++ handlers/dfgm_handler/src/main.rs
@@ -2,17 +2,31 @@
 Written By Devin Headrick
 Summer 2024
 
-All handlers:   
+Handler will    
     - handle all communication with their respective subsystem via interface 
     - communicate with other FSW via IPC 
     - take opcodes from messages passed to them, and execute their related functionality
+
+
+DFGM is a simple subsystem that only outputs a ~1250 byte packet at 1Hz, with no interface or control from the FSW. 
+The handler either chooses to collect the data or not. 
+
 */
 
+/// Opcodes for messages relating to DFGM functionality
+pub enum OpCode {
+   ToggleDataCollection, // toggles a flag which either enables or disables data collection from the DFGM 
+}
 
-// Trait for all handlers
+struct DFGMHandler {
+    toggle_data_collection: bool, 
+}
+
+/// on startup get value of toggle_data_collection from the State manager 
 
 fn main() {
-    println!("Hello, world!");
+    println!("Beginning DFGM Handler...");
+    
 }
 
 
