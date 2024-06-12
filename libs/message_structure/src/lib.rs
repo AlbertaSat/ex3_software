48c755d2f2ba7f87a8fd353ce1bb3be5dccf580d
Devin <headrick@ualberta.ca>
2024-06-12 14:00:43 -0600

    Modified test to use msg contructor. Tidied up code

--- libs/message_structure/src/lib.rs
+++ libs/message_structure/src/lib.rs
@@ -7,20 +7,18 @@ it flows through the ex3 software stack (GS to OBC and various software componen
 References:
     -
 */
-use serde::Serialize;
 use serde::Deserialize;
+use serde::Serialize;
 
 /// This message header is shared by all message types
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct MsgHeader {
-    pub msg_len: u8,
-    pub msg_id: u8, // This is a unique identifier for each message sent by the GS
-    pub dest_id: u8,   //The message dispatcher uses this to determine which destination to send this message
+    pub msg_len: u8,   // This is the length of the whole message (header + body) in bytes
+    pub msg_id: u8,    // This is a unique identifier for each message sent by the GS
+    pub dest_id: u8, //The message dispatcher uses this to determine which destination to send this message
     pub source_id: u8, // Identifies what sent the message for logging/debugging
     pub op_code: u8, // This is a unique code related to some specfic meaning for each destination
 }
-//
-
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Msg {
@@ -28,37 +26,17 @@ pub struct Msg {
     pub msg_body: Vec<u8>, //Contents of the msg body are dictated by the opcode
 }
 
-// Have methods to extract different values from a message
-// Getting specific values from the msg body based on byte offset is handled by the respective architecture component
-// The positions of the fields of the message header
-
-// Define methods that should be the same for all Message types
-
-    // members fxn to get values
-
-    ///TODO - fxn to compare t
 impl Msg {
-    //Builder that creates a message based on its 'type'
-    fn new(
-        msg_id: u8,
-        dest_id: u8,
-        source_id: u8,
-        opcode: u8,
-        data: Vec<u8>,
-    ) -> Self {
-        // Build message header first
+    // Constructor to create message header with correct length
+    fn new(msg_id: u8, dest_id: u8, source_id: u8, opcode: u8, data: Vec<u8>) -> Self {
         let len = data.len() as u8;
-
         let header = MsgHeader {
-            msg_len: len + 5,
+            msg_len: len + 5, //5 bytes for header fields
             msg_id: msg_id,
             dest_id: dest_id,
             source_id: source_id,
             op_code: opcode,
         };
-
-
-
         Msg {
             header,
             msg_body: data,
@@ -66,31 +44,24 @@ impl Msg {
     }
 }
 
-pub fn get_msg_body(msg: &Msg) -> Vec<u8> {
-    msg.msg_body.clone()
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::io::Cursor;
     use serde_json;
+    use std::io::Cursor;
 
     #[test]
     fn test_msg_serdes() {
-        let Msg = Msg {
-            header: MsgHeader { msg_len: 0 , msg_id: 0, dest_id: 0, source_id: 0, op_code: 0 },
-            msg_body: vec![0,1,2,3,4,5,6],
-        };
+        let msg = Msg::new(0, 2, 3, 4, vec![0, 1, 2, 3, 4, 5, 6]);
         let mut buf = Vec::new();
-        let serialized_msg = serde_json::to_writer(&mut buf, &Msg).unwrap();
+        let serialized_msg = serde_json::to_writer(&mut buf, &msg).unwrap();
         println!("Serde Msg: {:?}", buf);
 
         let mut cursor = Cursor::new(buf);
         let deserialized_msg: Msg = serde_json::from_reader(&mut cursor).unwrap();
 
         println!("Deserialized Msg: {:?}", deserialized_msg);
-        assert_eq!(deserialized_msg.header.msg_len,Msg.header.msg_len);
-        assert_eq!(deserialized_msg.msg_body,Msg.msg_body);
+        assert_eq!(deserialized_msg.header.msg_len, msg.header.msg_len);
+        assert_eq!(deserialized_msg.msg_body, msg.msg_body);
     }
 }
