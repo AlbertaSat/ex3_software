c571e8a8295cde414b9799f8a8035a7590bac434
rrasmuss07 <rrasmuss@ualberta.ca>
2024-06-14 14:04:15 -0600

    deserializes a message

--- handlers/bulk_msg_handler/src/main.rs
+++ handlers/bulk_msg_handler/src/main.rs
@@ -8,11 +8,8 @@ use common::ports::BULK_MSG_HANDLER_DISPATCHER_PORT;
  */
 use interfaces::*;
 use message_structure::*;
-use common::*;
-use std::char::MAX;
-use std::io::Cursor;
 use std::sync::mpsc;
-const MAX_BODY_SIZE: u8 = 128;
+const MAX_BODY_SIZE: usize = 128;
 fn main() {
     run_bulk_msg_handler();
 }
@@ -22,27 +19,25 @@ fn run_bulk_msg_handler() {
     let port = BULK_MSG_HANDLER_DISPATCHER_PORT;
     let tcp_interface = interfaces::TcpInterface::new_server(ip, port).unwrap();
 
-    let (sched_reader_tx, sched_reader_rx) = mpsc::channel();
-    // let (sched_writer_tx, sched_writer_rx) = mpsc::channel();
+    let (bulk_reader_tx, bulk_reader_rx) = mpsc::channel();
+    // let (bulk_writer_tx, bulk_writer_rx) = mpsc::channel();
 
-    interfaces::async_read(tcp_interface.clone(), sched_reader_tx, TCP_BUFFER_SIZE);
-
+    interfaces::async_read(tcp_interface.clone(), bulk_reader_tx, TCP_BUFFER_SIZE);
     loop {
-        if let Ok(buffer) = sched_reader_rx.recv() {
-            // Trimming trailing 0's so JSON doesn't give a "trailing characters" error
-            let trimmed_buffer: Vec<_> = buffer.into_iter().take_while(|&x| x != 0).collect();
-            let mut cursor = Cursor::new(trimmed_buffer);
-            let deserialized_msg: Msg = serde_json::from_reader(&mut cursor).unwrap();
+        let mut body_len: usize = 0;
+        if let Ok(buffer) = bulk_reader_rx.recv() {
+            let deserialized_msg: Msg = deserialize_msg(buffer).unwrap();
+            // len() returns the length in bytes here since each element is a u8
+            body_len =  deserialized_msg.msg_body.len();
 
-            // gets size of msg
-            let body_size: u64 = deserialized_msg.msg_body.len(); // this is # of bytes since each element is a u8
-
-            if body_size <= MAX_BODY_SIZE {
-                // let packet go
+            if body_len <= MAX_BODY_SIZE {
+                // write to stream
             } else {
-                break_packet();
+                chop_msg()
             }
-
+        } else {
+            eprintln!("Failed to read Msg struct");
+        }
         }
     }
-        }
+
