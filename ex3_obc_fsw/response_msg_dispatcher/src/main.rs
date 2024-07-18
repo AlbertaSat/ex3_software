81c5658117ab9827bbd822b15b5ade80b626b570
Rowan <rowan.rasmusson@gmail.com>
2024-07-18 10:38:12 -0600

    Unix domain server now polls and accepts connections.

--- ex3_obc_fsw/response_msg_dispatcher/src/main.rs
+++ ex3_obc_fsw/response_msg_dispatcher/src/main.rs
@@ -1,40 +1,58 @@
 use ipc_interface::*;
 use common::*;
 use message_structure::*;
+use std::collections::HashSet;
 
 fn main() -> Result<(), std::io::Error> {
     // Setup communication with handler(s) and coms_handler
     let dfgm_interface = IPCInterface::new_server("bulk_dfgm".to_string())?;
-    let mut dfgm_ipc_initial_buf = vec![0u8;128];
-    let mut dfgm_ipc_num_bytes_read: usize = 0;
+    let mut connected_clients: HashSet<i32> = HashSet::new();
 
 
-    loop{
-        // Poll both the UHF transceiver and IPC unix domain socket for the GS channel
-        let dfgm_ipc_bytes_read_result = read_socket(dfgm_interface.fd, &mut dfgm_ipc_initial_buf);
-        match dfgm_ipc_bytes_read_result {
-            Ok(num_bytes_read) => {
-                dfgm_ipc_num_bytes_read = num_bytes_read;
-
+    loop {
+        match dfgm_interface.accept_connection() {
+            Ok(client_fd) => {
+                println!("New client accepted on fd: {}", client_fd);
+                connected_clients.insert(client_fd);
             }
             Err(e) => {
-                println!("Error reading from DFGM IPC socket: {:?}", e);
+                println!("Error accepting connection: {}", e);
             }
         }
 
-        if dfgm_ipc_num_bytes_read > 0 {
-            println!("Received DFGM IPC Msg bytes");
-            let deserialized_msg_result = deserialize_msg(&dfgm_ipc_initial_buf.as_slice());
-            match deserialized_msg_result {
-                Ok(deserialized_msg) => {
-                    todo!()
+        // Poll existing connections
+        let mut to_remove = Vec::new();
+        for &client_fd in &connected_clients {
+            let mut ipc_initial_buf = vec![0u8;128];
+            let ipc_bytes_read_result = read_socket(client_fd, &mut ipc_initial_buf);
+            match ipc_bytes_read_result {
+                Ok(num_bytes_read) => {
+                    if num_bytes_read > 0 {
+                        println!("Received {} IPC Msg bytes", num_bytes_read);
+                        // Deserial Msg to look at body - path to data
+                        let deserialized_msg_result = deserialize_msg(&ipc_initial_buf.as_slice());
+                        match deserialized_msg_result {
+                            Ok(deserialized_msg) => {
+                                // Fetch data from path in body. Get string from bytes in body
+                                todo!()
+                            }
+                            Err(e) => {
+                                println!("Error deserializing GS IPC msg: {}", e);
+                            }
+                        };
+                    }
                 }
                 Err(e) => {
-                    println!("Error deserializing GS IPC msg: {:?}", e);
-                    //Handle deserialization of IPC msg failure
+                    println!("Error reading from DFGM IPC socket: {:?}", e);
+                    if e.kind() == std::io::ErrorKind::UnexpectedEof {
+                        println!("Client disconnected: {}", client_fd);
+                        to_remove.push(client_fd);
+                    }
                 }
-            };
+            }
+        }
+        for client_fd in to_remove {
+            connected_clients.remove(&client_fd);
         }
     }
-    Ok(())
 }
