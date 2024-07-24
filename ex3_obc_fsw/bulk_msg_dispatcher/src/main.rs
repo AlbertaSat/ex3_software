d1cb881dad69523053a3fb662aa621d732ed4a31
Rowan <rowan.rasmusson@gmail.com>
2024-07-24 14:24:46 -0600

    Changed path of dfgm_data write. Got gs hooked up to bulk_disp.

--- ex3_obc_fsw/bulk_msg_dispatcher/src/main.rs
+++ ex3_obc_fsw/bulk_msg_dispatcher/src/main.rs
@@ -5,8 +5,9 @@ use message_structure::*;
 fn main() -> Result<(), std::io::Error> {
     // All connected handlers and other clients will have a socket for the server defined here
     let mut dfgm_interface = IpcServer::new("dfgm_bulk".to_string())?;
+    let mut gs_interface = IpcServer::new("gs_bulk".to_string())?;
 
-    let mut servers: Vec<&mut IpcServer> = vec![&mut dfgm_interface];    
+    let mut servers: Vec<&mut IpcServer> = vec![&mut dfgm_interface, &mut gs_interface];    
     loop {
         poll_ipc_server_sockets(&mut servers);
         
@@ -24,7 +25,7 @@ fn handle_client(server: &mut IpcServer) -> Result<(), std::io::Error> {
             &server.buffer
         );
         //TMP - test read/write to dfgm
-        ipc_write(server.data_fd, server.buffer.as_slice())?;
+        // ipc_write(server.data_fd, server.buffer.as_slice())?;
         server.clear_buffer();
     }
     Ok(())
