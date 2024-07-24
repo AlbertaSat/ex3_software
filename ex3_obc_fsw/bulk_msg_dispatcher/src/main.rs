68265c0b6fbd9a5e957c713bb8c1f323ab692b08
Rowan <rowan.rasmusson@gmail.com>
2024-07-24 10:57:34 -0600

    changed Cargo.toml's of dfgm and bulk.

--- ex3_obc_fsw/bulk_msg_dispatcher/src/main.rs
+++ ex3_obc_fsw/bulk_msg_dispatcher/src/main.rs
@@ -1,41 +1,24 @@
 use ipc_interface::*;
+use ipc::*;
 use common::*;
 use message_structure::*;
 
 fn main() -> Result<(), std::io::Error> {
-    let dfgm_interface = IPCServerInterface::new_server("dfgm_bulk".to_string())?;
-    let gs_interface = IPCServerInterface::new_server("gs_bulk".to_string())?;
-    let mut connected_clients: Vec<IPCServerInterface> = vec![dfgm_interface, gs_interface.clone()];
-    let socket_buf: &mut Vec<u8> = &mut vec![0u8; IPC_BUFFER_SIZE];
-    let arb_data = Msg::new(0,0,0,0,0,"<path>".as_bytes().to_vec());
+    // All connected handlers and other clients will have a socket for the server defined here
+    let mut dfgm_interface = IpcServer::new("dfgm_bulk".to_string())?;
+
+
     loop {
-        poll_server_interfaces(&mut connected_clients, socket_buf);
+        let mut servers: Vec<&mut IpcServer> = vec![&mut dfgm_interface];
+        poll_ipc_server_sockets(servers);
+        for server in servers {
+            handle_client(server);
+        }
     }
 }
 
 
-fn handle_client(client_fd: i32) -> Result<(), std::io::Error> {
-    let mut ipc_initial_buf = vec![0u8; 128];
-    match read_socket(client_fd, &mut ipc_initial_buf) {
-        Ok(num_bytes_read) => {
-            if num_bytes_read > 0 {
-                println!("Received {} IPC Msg bytes", num_bytes_read);
-                let deserialized_msg = deserialize_msg(&ipc_initial_buf.as_slice())?;
-                // TMP - testing sending and clients ability to read
-                send_over_socket(client_fd, serialize_msg(&deserialized_msg)?)?;
-                println!("Sent {:?}", deserialized_msg);
-                // match deserialized_msg_result {
-                        //     Ok(deserialized_msg) => {
-                        //         // Fetch data from path in body. Get string from bytes in body
-                        //         todo!()
-                        //     }
-                        //     Err(e) => {
-                        //         println!("Error deserializing GS IPC msg: {}", e);
-                        //     }
-                        // };
-            }
-        }
-        Err(e) => return Err(e),
-    }
+fn handle_client(server: &mut IpcServer) -> Result<(), std::io::Error> {
+
     Ok(())
 }
