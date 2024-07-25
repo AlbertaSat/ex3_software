3dade51aa5e26d5ba6d049bf88dfdacdd652f22d
Rowan <rowan.rasmusson@gmail.com>
2024-07-25 11:15:20 -0600

    Changed name of bulk msg lib in shared libs. Everything works so far up to sending Msg's to gs handler.

--- ex3_obc_fsw/bulk_msg_dispatcher/src/main.rs
+++ ex3_obc_fsw/bulk_msg_dispatcher/src/main.rs
@@ -2,26 +2,43 @@ use ipc::*;
 use std::fs::{OpenOptions, File};
 use std::io::Read;
 use common::*;
+use bulk_msg_slicing::*;
 use message_structure::*;
 use std::io::Error as IoError;
 
 fn main() -> Result<(),IoError > {
     // All connected handlers and other clients will have a socket for the server defined here
     let mut dfgm_interface: IpcServer = IpcServer::new("dfgm_bulk".to_string())?;
-    let mut gs_interface: IpcServer = IpcServer::new("gs_bulk".to_string())?;
+    let gs_interface: IpcServer = IpcServer::new("gs_bulk".to_string())?;
+    let mut gs_interface_clone: IpcServer = gs_interface.clone();
 
-    let mut servers: Vec<&mut IpcServer> = vec![&mut dfgm_interface, &mut gs_interface];    
+    let mut servers: Vec<&mut IpcServer> = vec![&mut dfgm_interface, &mut gs_interface_clone];    
     loop {
         poll_ipc_server_sockets(&mut servers);
         
         for server in &mut servers {
-            handle_client(server)?;
+            if let Some(msg) = handle_client(server)? {
+                if msg.msg_body.len() > MAX_SINGLE_BODY_SIZE && msg.header.msg_type == MsgType::Bulk as u8 {
+                    let path_bytes: Vec<u8> = msg.msg_body;
+                    let mut path: &str = std::str::from_utf8(&path_bytes).expect("Found invalid UTF-8 in path.");
+                    path = path.trim_matches(char::from(0));
+                    println!("Got path: {}", path);
+                    let bulk_msg: Msg = get_data_from_path(&path)?;
+                    // Slice bulk msg
+                    let messages: Vec<Msg> = handle_large_msg(bulk_msg)?;
+
+                    // Start coms protocol with GS handler to downlink
+                    send_bulk_msg_to_gs(messages, gs_interface.data_fd)?;
+
+                    server.clear_buffer();
+                }
+            }
         }
     }
 }
 
 /// In charge of getting the file path from a Msg sent to the Bulk dispatcher from a handler
-fn handle_client(server: &mut IpcServer) -> Result<(), IoError> {
+fn handle_client(server: &IpcServer) -> Result<Option<Msg>, IoError> {
     if server.buffer != [0u8; IPC_BUFFER_SIZE] {
         println!(
             "Server {} received data: {:?}",
@@ -29,31 +46,19 @@ fn handle_client(server: &mut IpcServer) -> Result<(), IoError> {
             &server.buffer
         );
         //Build Msg from received bytes and get body which contains path
-        let msg: Msg = deserialize_msg(&server.buffer)?;
-        let path_bytes: Vec<u8> = msg.msg_body;
-        let mut path = std::str::from_utf8(&path_bytes).expect("Found invalid UTF-8 in path.");
-        path = path.trim_matches(char::from(0));
-        println!("Got path: {}", path);
-        let bulk_msg: Msg = get_data_from_path(&path)?;
-        // Slice bulk msg
-
-
-        // Start coms protocol with GS handler to downlink
-
-
-        server.clear_buffer();
+        Ok(Some(deserialize_msg(&server.buffer)?))
+    } else {
+        Ok(None)
     }
-    Ok(())
 }
-
-
 
 /// This is the communication protocol that will execute each time the Bulk Msg Dispatcher wants
 /// to send a Bulk Msg to the GS handler for downlinking.
 fn send_bulk_msg_to_gs(messages: Vec<Msg>, fd: Option<i32>) -> Result<(), IoError> {
+    println!("Exectuing bulk msg sending protocol");
     // 1. Send CmdMsg to GS handler indicating Bulk Msg and buffer size needed
-    let buffer_msg = CmdMsg::new(0,9,10,0,"dummy".as_bytes().to_vec());
-    ipc_write(fd, &buffer_msg.serialize_to_bytes()?)?;
+    // let buffer_msg = CmdMsg::new(0,9,10,0,"dummy".as_bytes().to_vec());
+    // ipc_write(fd, &buffer_msg.serialize_to_bytes()?)?;
 
     // 2. Wait for ACK from GS handler
     // 3. Send Msg's contained in messages
