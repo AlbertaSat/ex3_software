2705fbc84330bc644c309e7c5866954120fec147
Devin <headrick@ualberta.ca>
2024-07-25 19:41:59 -0600

    Setup ipc server dev tool for testing dummy handler

---
+++ ex3_obc_fsw/dev_tools/ipc_server_dev_tool/src/main.rs
@@ -0,0 +1,77 @@
+/*
+Written by Devin Headrick
+Summer 2024
+
+Create an ipc server on the path specified as an arg - and send hardcoded data based on user input to stdin
+
+*/
+
+use ipc::{ipc_write, poll_ipc_server_sockets, IpcServer, IPC_BUFFER_SIZE};
+
+use nix::poll::{poll, PollFd, PollFlags};
+use std::io::{self, Read};
+
+const STDIN_POLL_TIMEOUT_MS: i32 = 100;
+
+fn handle_user_input(read_data: &[u8]) {
+    let first_byte = read_data[0];
+    let first_byte_char = first_byte as char;
+    println!("First byte: {}", first_byte_char);
+
+    match first_byte_char {
+        '1' => {
+            println!("Sending msg 1");
+            //write first hardcoded msg to ipc client 
+        }
+        '2' => println!("Sending msg 2"),
+        '3' => println!("Sending msg 3"),
+        _ => println!("Invalid input"),
+    }
+}
+
+fn poll_stdin(mut buffer: &mut [u8]) -> Option<usize> {
+    let stdin_fd = 0; // File descriptor for stdin is always 0
+    let mut poll_fds = [PollFd::new(stdin_fd, PollFlags::POLLIN)];
+
+    match poll(&mut poll_fds, STDIN_POLL_TIMEOUT_MS) {
+        Ok(1) if poll_fds[0].revents().unwrap().contains(PollFlags::POLLIN) => {
+            match io::stdin().read(&mut buffer) {
+                Ok(bytes_read) => Some(bytes_read),
+                Err(_) => None,
+            }
+        }
+        _ => None,
+    }
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: cargo run --bin ipc_server_dev_tool <path to ipc socket>");
+        std::process::exit(1);
+    }
+
+    let mut ipc_server = IpcServer::new(args[1].clone()).unwrap();
+
+    loop {
+        poll_ipc_server_sockets(vec![&mut ipc_server]);
+        if ipc_server.buffer != [0u8; IPC_BUFFER_SIZE] {
+            println!("Received message from ipc client {:?}", ipc_server.buffer);
+            ipc_server.clear_buffer();
+        }
+
+        // Poll stdin for user input
+        let mut stdin_buf = [0u8; 1024]; // Adjust buffer size as needed
+        let stdin_read_res = poll_stdin(&mut stdin_buf);
+        match stdin_read_res {
+            Some(bytes_read) => {
+                if bytes_read > 0 {
+                    println!("Received user input: {:?}", stdin_buf);   
+                    handle_user_input(stdin_buf.as_slice());
+                }
+            }
+            None => (),
+        }
+    }
+}
