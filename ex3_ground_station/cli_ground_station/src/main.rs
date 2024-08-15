b1b1290564a5c384ccf57859579b9585594a0de9
Rowan <rowan.rasmusson@gmail.com>
2024-08-15 14:20:10 -0600

    GS builds different msg for bulk downlink.

--- ex3_ground_station/cli_ground_station/src/main.rs
+++ ex3_ground_station/cli_ground_station/src/main.rs
@@ -14,7 +14,9 @@ TODO
 
 use bulk_msg_slicing::*;
 use common::component_ids::*;
+use common::opcodes::dfgm::GET_DFGM_DATA;
 use common::ports::SIM_COMMS_PORT;
+use serde_json::de;
 use core::num;
 use libc::c_int;
 use message_structure::*;
@@ -80,11 +82,21 @@ fn build_msg_from_operator_input(operator_str: String) -> Result<Msg, std::io::E
             "Not enough arguments",
         ));
     }
-    let dest_id = ComponentIds::from_str(operator_str_split[0]).unwrap() as u8;
+    let mut dest_id = ComponentIds::from_str(operator_str_split[0]).unwrap() as u8;
     let opcode = operator_str_split[1].parse::<u8>().unwrap();
     let mut msg_body: Vec<u8> = Vec::new();
-    for data_byte in operator_str_split[2..].into_iter() {
+
+    // This is for the Bulk Msg Disp to parse and determine the path it needs to use to get the data
+    if dest_id == DFGM {
+        if opcode == GET_DFGM_DATA {
+            dest_id = ComponentIds::BULK_MSG_DISPATCHER as u8;
+            let body = "DFGM DATA".as_bytes();
+            msg_body.extend(body);
+        }
+    } else {
+        for data_byte in operator_str_split[2..].into_iter() {
         msg_body.push(data_byte.parse::<u8>().unwrap());
+        }
     }
     let msg = Msg::new(0, 0, dest_id, GS, opcode, msg_body);
     println!("Built msg: {:?}", msg);
