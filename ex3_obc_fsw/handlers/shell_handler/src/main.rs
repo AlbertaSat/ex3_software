ddcf73b32c55be5a38b656ecbb5ddcdc2efa0c4b
Kaaden RumanCam <kaadenruman@hotmail.com>
2024-08-18 01:43:35 -0400

    outline of shell handler

--- ex3_obc_fsw/handlers/shell_handler/src/main.rs
+++ ex3_obc_fsw/handlers/shell_handler/src/main.rs
@@ -1,74 +1,48 @@
 /*
-Written by Kaaden RumanCam and Ron Unrau
+Written by Kaaden RumanCam
 Summer 2024
 
 ...
 
-TODO - If connection is lost with an interface, attempt to reconnect every 5 seconds
-TOOD - Figure out way to use polymorphism and have the interfaces be configurable at runtime (i.e. TCP, UART, etc.)
-TODO - Get state variables from a state manager (channels?) upon instantiation and update them as needed.
-TODO - Setup a way to handle opcodes from messages passed to the handler
-
+TODO
 */
 
-use std::io::Error;
-use ipc::{poll_ipc_clients, IpcClient, IPC_BUFFER_SIZE};
-use log::{debug, trace, warn};
-use message_structure::*;
+//use common::{opcodes, ports};
+use ipc::{IpcClient, poll_ipc_clients};
 
 struct ShellHandler {
-    msg_dispatcher_interface: Option<IpcClient>, // For communcation with other FSW components [internal to OBC]
+    msg_dispatcher_interface: IpcClient, // To get messages from the dispatcher
 }
 
 impl ShellHandler {
     pub fn new(
-        msg_dispatcher_interface: Result<IpcClient, std::io::Error>,
+        msg_dispatcher_interface: IpcClient,
     ) -> ShellHandler {
-        if msg_dispatcher_interface.is_err() {
-            warn!(
-                "Error creating dispatcher interface: {:?}",
-                msg_dispatcher_interface.as_ref().err().unwrap()
-            );
-        }
         ShellHandler {
-            msg_dispatcher_interface: msg_dispatcher_interface.ok(),
+            msg_dispatcher_interface,
         }
     }
 
-    // Sets up threads for reading and writing to its interaces, and sets up channels for communication between threads and the handler
     pub fn run(&mut self) -> std::io::Result<()> {
         // Poll for messages
         loop {
-            let msg_dispatcher_interface = self.msg_dispatcher_interface.as_mut().expect("msg_dispatcher_interface has value of None");
-
             let mut clients = vec![
-                msg_dispatcher_interface,
+                &mut self.msg_dispatcher_interface,
             ];
             poll_ipc_clients(&mut clients)?;
 
-            // Handling the bulk message dispatcher interface
-            let msg_dispatcher_interface = self.msg_dispatcher_interface.as_ref().unwrap();
-            if msg_dispatcher_interface.buffer != [0u8; IPC_BUFFER_SIZE] {
-                let recv_msg: Msg = deserialize_msg(&msg_dispatcher_interface.buffer).unwrap();
-                debug!("Received and deserialized msg");
-                self.handle_msg(recv_msg)?;
-            }
-        }
-    }
-    //TODO - After receiving the message, send a response back to the dispatcher ??
 
-    fn handle_msg(&mut self, msg: Msg) -> Result<(), Error> {
-        self.msg_dispatcher_interface.as_mut().unwrap().clear_buffer();
-        println!("SHELL msg opcode: {} {:?}", msg.header.op_code, msg.msg_body);
-        Ok(())
+        }
     }
 }
 
 fn main() {
-    trace!("Starting Shell Handler...");
+    println!("Beginning Shell Handler...");
 
-    // Create Unix domain socket interface for to talk to message dispatcher
-    let msg_dispatcher_interface = IpcClient::new("shell_handler".to_string());
+    let msg_dispatcher_interface = match IpcClient::new("shell_handler".to_string()) {
+        Ok(mdi) => mdi,
+        Err(e) => panic!("Error creating dispatcher interface: {e:?}"),
+    };
 
     let mut shell_handler = ShellHandler::new(msg_dispatcher_interface);
 
