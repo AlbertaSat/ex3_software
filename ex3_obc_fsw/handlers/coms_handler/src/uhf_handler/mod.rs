293803a688d222da01c70ad5f9a4123c6e47b43f
Drake <dboul63@gmail.com>
2024-08-25 10:47:47 -0600

    Refactored code and fixed bugs

--- ex3_obc_fsw/handlers/coms_handler/src/uhf_handler/mod.rs
+++ ex3_obc_fsw/handlers/coms_handler/src/uhf_handler/mod.rs
@@ -1,5 +1,7 @@
 use std::fs::read;
+use std::thread::Thread;
 
+use common::component_ids::GS;
 /*
 Written by Drake Boulianne
 Summer 2024
@@ -16,127 +18,101 @@ use common::opcodes;
 use common::constants::UHF_MAX_MESSAGE_SIZE_BYTES;
 use common::ports;
 use common::component_ids::ComponentIds::UHF;
+use std::thread;
+use std::time::Duration;
 
 
-
-fn set_beacon_value(interface: &mut TcpInterface, new_beacon_value: Vec<u8>) -> Vec<u8>{
-    // TODO - write this data to the UHF beacon buffer (or however it actually works w/ the hardware)
-    let prefix: Vec<u8> = "UHF:SET_BEACON:".as_bytes().to_vec();
-
-    // Temporary command format sent to UHF to modify beacon 
-    let mut content: Vec<u8> = new_beacon_value.clone();
-    content.splice(0..0, prefix);
-
-    // Send message to UHF
-    let send_result = interface.send(&content);
-    match send_result {
-        Ok(_) => println!("Send successful."),
-        Err(e) => println!("Error occured setting beacon value:  {:?}", e)
-    }
-    // Then get and return beacon value to confirm it was changed
-    get_beacon_value(interface)
-}
-
-fn get_beacon_value(interface: &mut TcpInterface) -> Vec<u8>{
-
-    let request = "UHF:GET_BEACON:".as_bytes().to_vec();
-    let mut buffer: Vec<u8> = Vec::new();
-    // Send message to UHF
-    let send_result = interface.send(&request);
-    match send_result {
-        Ok(_) => {},
-        Err(e) => println!("Error occured getting beacon value: {:?}", e)
-    }
-    // loop awaiting for message. TODO: This should timeout after a while...
-    loop {
-        if let Ok(n) = TcpInterface::read(interface, &mut buffer) {
+fn read_buffer(interface: &mut TcpInterface) -> Vec<u8> {
+    let mut buffer: Vec<u8> = vec![0; UHF_MAX_MESSAGE_SIZE_BYTES as usize]; //Buffer to read incoming messages from UHF
+    let read_result: Result<usize, std::io::Error> = TcpInterface::read(interface, &mut buffer);
+    match read_result {
+        Ok(n) => {
             if n > 0 {
-                return buffer
+                return buffer.to_vec()
             } else {
-                continue;
+                return buffer.to_vec()
             }
-        } else {
-            println!("No bytes to read");
+        }, 
+        Err(_) => {
+            return "Failed to read".as_bytes().to_vec()
         }
     }
 }
 
-fn set_mode(interface: &mut TcpInterface, new_mode: Vec<u8>) -> Vec<u8> {
-    let prefix: Vec<u8> = "UHF:SET_MODE:".as_bytes().to_vec();
 
-    let mut content: Vec<u8> = new_mode.clone();
-    content.splice(0..0, prefix);
-
-    // Send message to UHF
+fn send_msg(interface: &mut TcpInterface, content: Vec<u8>) {
     let send_result = interface.send(&content);
     match send_result {
         Ok(_) => println!("Send successful."),
         Err(e) => println!("Error occured setting beacon value:  {:?}", e)
     }
-    get_mode(interface)
+}
+
+fn extract_non_null_bytes(buffer: Vec<u8>) -> Vec<u8> {
+    // function used for testing. Takes vector of bytes and returns array without null characters. 
+    let mut useful_msg: Vec<u8> = Vec::new();
+    for byte in buffer {
+        if byte == b'\0' {
+            continue;
+        } else {
+            useful_msg.push(byte)
+        }
+    }
+    useful_msg
+}
+
+fn set_beacon_value(interface: &mut TcpInterface, new_beacon_value: Vec<u8>) -> Vec<u8>{
+    // Construct command for simulated UHF
+    let prefix: Vec<u8> = "UHF:SET_BEACON:".as_bytes().to_vec();
+    let mut cmd: Vec<u8> = new_beacon_value.clone();
+    cmd.splice(0..0, prefix);
+
+    //Send the command
+    send_msg(interface, cmd);
+    // Read response from the simulated UHF and return it.
+    read_buffer(interface)
+}
+
+fn get_beacon_value(interface: &mut TcpInterface) -> Vec<u8>{
+    let request = "UHF:GET_BEACON:".as_bytes().to_vec();
+
+    send_msg(interface, request);
+    read_buffer(interface)
+}
+
+
+fn set_mode(interface: &mut TcpInterface, new_mode: Vec<u8>) -> Vec<u8> {
+    let prefix: Vec<u8> = "UHF:SET_MODE:".as_bytes().to_vec();
+    let mut cmd: Vec<u8> = new_mode.clone();
+    cmd.splice(0..0, prefix);
+
+    send_msg(interface, cmd);
+    read_buffer(interface)
 }
 
 fn get_mode(interface: &mut TcpInterface) -> Vec<u8> {
     let request = "UHF:GET_MODE:".as_bytes().to_vec();
-    let mut buffer: Vec<u8> = Vec::new();
-    // Send message to UHF
-    let send_result = interface.send(&request);
-    match send_result {
-        Ok(_) => {},
-        Err(e) => println!("Error occured getting beacon value: {:?}", e)
-    }
-    // loop awaiting for message. TODO: This should timeout after a while...
-    loop {
-        if let Ok(n) = TcpInterface::read(interface, &mut buffer) {
-            if n > 0 {
-                return buffer
-            } else {
-                continue;
-            }
-        } else {
-            println!("No bytes to read");
-        }
-    }
+
+    send_msg(interface, request);
+    read_buffer(interface)
 }
 
 
 fn set_baud_rate(interface: &mut TcpInterface, new_baud_rate: Vec<u8>) -> Vec<u8>{
     let prefix: Vec<u8> = "UHF:SET_MODE:".as_bytes().to_vec();
+    let mut cmd: Vec<u8> = new_baud_rate.clone();
+    cmd.splice(0..0, prefix);
 
-    let mut content: Vec<u8> = new_baud_rate.clone();
-    content.splice(0..0, prefix);
-
-    // Send message to UHF
-    let send_result = interface.send(&content);
-    match send_result {
-        Ok(_) => println!("Send successful."),
-        Err(e) => println!("Error occured setting beacon value:  {:?}", e)
-    }
-    get_baud_rate(interface)
+    send_msg(interface, cmd);
+    read_buffer(interface)
 }
 
 
 fn get_baud_rate(interface: &mut TcpInterface) -> Vec<u8> {
     let request = "UHF:GET_BAUD_RATE:".as_bytes().to_vec();
-    let mut buffer: Vec<u8> = Vec::new();
-    // Send message to UHF
-    let send_result = interface.send(&request);
-    match send_result {
-        Ok(_) => {},
-        Err(e) => println!("Error occured getting beacon value: {:?}", e)
-    }
-    // loop awaiting for message. TODO: This should timeout after a while...
-    loop {
-        if let Ok(n) = TcpInterface::read(interface, &mut buffer) {
-            if n > 0 {
-                return buffer
-            } else {
-                continue;
-            }
-        } else {
-            println!("No bytes to read");
-        }
-    }
+
+    send_msg(interface, request);
+    read_buffer(interface)
 }
 
 
@@ -146,6 +122,7 @@ fn get_hk_data(interface: &mut TcpInterface, content: Vec<u8>) -> Vec<u8> {
 }
 
 fn reset_uhf(interface: &mut TcpInterface) -> Vec<u8> {
+    // Just returns "Resetting UHF" for now
     let bytes: Vec<u8> = vec![
     0x52, 0x65, 0x73, 0x65, 0x74, 0x74, 0x69, 0x6E, 0x67, 0x20, 
     0x55, 0x48, 0x46, 0x2E, 0x2E, 0x2E
@@ -155,7 +132,7 @@ fn reset_uhf(interface: &mut TcpInterface) -> Vec<u8> {
 
 }
 
-pub fn handle_uhf_cmd(interface: &mut TcpInterface, msg: &Msg) -> Vec<u8>{
+pub fn handle_uhf_cmd(interface: &mut TcpInterface, msg: &Msg) -> Vec<u8> {
     let opcode = opcodes::UHF::from(msg.header.op_code);
     
     match opcode {
@@ -174,6 +151,12 @@ pub fn handle_uhf_cmd(interface: &mut TcpInterface, msg: &Msg) -> Vec<u8>{
         opcodes::UHF::GetBaudRate => {
             get_baud_rate(interface)
         },
+        opcodes::UHF::SetMode => {
+            set_mode(interface, msg.msg_body.clone())
+        },
+        opcodes::UHF::GetMode => {
+            get_mode(interface)
+        }
         opcodes::UHF::Reset => {
             reset_uhf(interface)
         },
@@ -192,33 +175,67 @@ pub fn handle_uhf_cmd(interface: &mut TcpInterface, msg: &Msg) -> Vec<u8>{
 fn test_setting() {
     /*
     This is a test for the functionality of setting simulated parameters on the simulated UHF.
-    In this test we start running the test_setting.sh script which fires up the simulated UHF and a GS terminal.
-    Next the test_setting function initiates the Tcp interface and creates new simulated parameters to be sent as strings.
-    For each simulated parameter to be set, a msg is constructed, the command is then handled, and then the buffer
+    In this test we start by running the test.sh script which fires up the simulated UHF and a GS terminal.
+    See README.md For more information on running the test script.
+
+    Next the test_setting function initiates the Tcp interface and creates the new simulated parameters as strings.
+    For each simulated parameter to set, a msg is constructed, the command is then handled, and then the buffer
     is read into a variable. an assert statement then checks the values within the vector returned
     
-     */
-
+    */
 
     let mut interface = TcpInterface::new_client("127.0.0.1".to_string(), ports::SIM_COMMS_PORT).unwrap();
-    let new_beacon: String = String::from("bacon");
+
+    let new_beacon: String = String::from("new beacon");
     let new_mode: String = String::from("9");
     let new_baud_rate: String = String::from("4550");
 
     // Test setting beacon value
-    let msg: Msg = Msg::new(0, 0, UHF.into(), UHF.into(), 200, new_beacon.clone().into_bytes());
-    let returned: Vec<u8> = handle_uhf_cmd(&mut interface, &msg);
+    let msg: Msg = Msg::new(0, 0, UHF.into(), UHF.into(), opcodes::UHF::SetBeacon.into(), new_beacon.clone().into_bytes());
+    let returned: Vec<u8> = extract_non_null_bytes(handle_uhf_cmd(&mut interface, &msg));
     assert_eq!(new_beacon, String::from_utf8(returned).unwrap());
 
     // Test setting mode
-    let msg: Msg = Msg::new(0, 0, UHF.into(), UHF.into(), 200, new_mode.clone().into_bytes());
-    let returned: Vec<u8> = handle_uhf_cmd(&mut interface, &msg);
+    let msg: Msg = Msg::new(0, 0, UHF.into(), UHF.into(), opcodes::UHF::SetMode.into(), new_mode.clone().into_bytes());
+    let returned: Vec<u8> = extract_non_null_bytes(handle_uhf_cmd(&mut interface, &msg));
     assert_eq!(new_mode, String::from_utf8(returned).unwrap());
 
     // Test setting baud rate
-    let msg: Msg = Msg::new(0, 0, UHF.into(), UHF.into(), 200, new_baud_rate.clone().into_bytes());
-    let returned: Vec<u8> = handle_uhf_cmd(&mut interface, &msg);
+    let msg: Msg = Msg::new(0, 0, UHF.into(), UHF.into(), opcodes::UHF::SetBaudRate.into(), new_baud_rate.clone().into_bytes());
+    let returned: Vec<u8> = extract_non_null_bytes(handle_uhf_cmd(&mut interface, &msg));
     assert_eq!(new_baud_rate, String::from_utf8(returned).unwrap());
 
 
+}
+
+#[test]
+fn test_getting(){
+    /*
+    This is a test for the functionality of getting simulated parameters on the simulated UHF.
+    In this test we start by running the test.sh script which fires up the simulated UHF and a GS terminal.
+    See README.md For more information on running the test script.
+
+    Next the test_getting function initiates the Tcp interface and creates the expected simulated parameters to be sent as strings.
+    For each simulated parameter to get, a msg is constructed, the command is then handled, and then the buffer
+    is read into a variable. an assert statement then checks the values within the vector returned
+    
+    */
+
+    let mut interface = TcpInterface::new_client("127.0.0.1".to_string(), ports::SIM_COMMS_PORT).unwrap();
+
+    let expected_beacon: String = String::from("beacon");
+    let expected_mode: String = String::from("0");
+    let expected_baud_rate: String = String::from("9600");
+
+    let msg: Msg = Msg::new(0, 0, UHF.into(), GS.into(), opcodes::UHF::GetBeacon.into(), vec![]);
+    let returned = extract_non_null_bytes(handle_uhf_cmd(&mut interface, &msg));
+    assert_eq!(expected_beacon, String::from_utf8(returned).unwrap());
+
+    let msg: Msg = Msg::new(0, 0, UHF.into(), GS.into(), opcodes::UHF::GetMode.into(), vec![]);
+    let returned = extract_non_null_bytes(handle_uhf_cmd(&mut interface, &msg));
+    assert_eq!(expected_mode, String::from_utf8(returned).unwrap());
+
+    let msg: Msg = Msg::new(0, 0, UHF.into(), GS.into(), opcodes::UHF::GetBaudRate.into(), vec![]);
+    let returned = extract_non_null_bytes(handle_uhf_cmd(&mut interface, &msg));
+    assert_eq!(expected_baud_rate, String::from_utf8(returned).unwrap());
 }
