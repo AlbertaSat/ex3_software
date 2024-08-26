4b6029404612cacac04b1413ddf3b94ac0c35235
Rowan <rowan.rasmusson@gmail.com>
2024-08-26 13:07:32 -0600

    Deleted old ipc lib

--- ex3_obc_fsw/dev_tools/ipc_burst_hardcoded/src/main.rs
+++ ex3_obc_fsw/dev_tools/ipc_burst_hardcoded/src/main.rs
@@ -12,17 +12,13 @@ Usage:
 */
 
 
-use ipc_interface::{IPCInterface, IPC_BUFFER_SIZE, read_socket, send_over_socket};
+use ipc::*;
 use message_structure::{Msg, serialize_msg};
 use common::*;
 
-fn main(){
-    // Get name of target from args 
-    let args: Vec<String> = std::env::args().collect();
-    
+fn main() {
     //Setup interface for comm with OBC FSW components (IPC), by acting as a client connecting to msg dispatcher server
-    let ipc_interface = IPCInterface::new_client("test_handler".to_string()).unwrap();
-    let mut ipc_buf = vec![0; IPC_BUFFER_SIZE]; //Buffer to read incoming messages from IPC
+    let ipc_interface = IpcClient::new("test_handler".to_string()).unwrap();
 
     // Define msg to send contents
     let msg_data = vec![0x01, 0x03, 0x0a, 0x00];
@@ -32,7 +28,7 @@ fn main(){
     println!("Attempting to send: {:?}", msg_bytes);
 
     // Send the msg
-    send_over_socket(ipc_interface.fd, msg_bytes).unwrap();
+    ipc_write(ipc_interface.fd, &msg_bytes).unwrap();
 
     println!("Sent successful");
 
