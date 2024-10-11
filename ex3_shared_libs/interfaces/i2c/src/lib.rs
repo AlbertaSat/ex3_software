0f7b159a0579ea998e3c9547cdc8ac4c5f529def
Drake <dboul63@gmail.com>
2024-10-11 10:04:05 -0600

    Added functions to read and write to system management bus

--- ex3_shared_libs/interfaces/i2c/src/lib.rs
+++ ex3_shared_libs/interfaces/i2c/src/lib.rs
@@ -2,7 +2,7 @@ extern crate i2cdev;
 
 use i2cdev::core::*;
 use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
-use std::io::{Error, Read, Write};
+use std::io::{Error, Write};
 
 // Interface to be implemented by all external interfaces
 pub trait Interface {
@@ -19,6 +19,8 @@ pub trait Interface {
 // is going to be written and/or read from
 pub struct I2CInterface {
     i2c: LinuxI2CDevice,
+    bus_path: String,
+    client_address: u16,
 }
 
 impl Interface for I2CInterface {
@@ -36,9 +38,50 @@ impl Interface for I2CInterface {
 }
 
 impl I2CInterface {
-    pub fn new(path: &str, slave_address: u16) -> Result<I2CInterface, LinuxI2CError> {
+    pub fn new(path: &str, client_address: u16) -> Result<I2CInterface, LinuxI2CError> {
         // Initalize i2c interface with path and then pass slave address
-        let i2c = LinuxI2CDevice::new(path, slave_address)?;
-        Ok(I2CInterface { i2c })
+        let i2c = LinuxI2CDevice::new(path, client_address)?;
+        Ok(I2CInterface {
+            i2c,
+            bus_path: path.to_string(),
+            client_address,
+        })
     }
+
+    fn send_sm(&mut self, register: u8, byte: u8) -> Result<(), LinuxI2CError> {
+        // Send data to the slave using the i2c interface
+        self.i2c.smbus_write_byte_data(register, byte)?;
+        Ok(())
+    }
+
+    fn read_sm(&mut self, address: u8) -> Result<u8, LinuxI2CError> {
+        // read data from the slave using the i2c interface
+        self.i2c.smbus_read_byte_data(address)
+    }
+
+    fn print_device_registers(&mut self) {
+        println!("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f");
+
+        for row in (0x00..=0xFF).step_by(16) {
+            print!("{:02X}: ", row);
+
+            for col in 0x00..=0x0F {
+                let register = row + col;
+                match self.read_sm(register) {
+                    Ok(byte) => print!(" {:?} ", byte),
+                    Err(e) => {
+                        println!("Error reading row byte at register: {}", register);
+                        println!("{e}");
+                        return;
+                    }
+                }
+            }
+            println!(); // New line at the end of each row
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
 }
