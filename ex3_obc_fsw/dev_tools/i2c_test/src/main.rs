9dec56ebfbaa97aef8054f1b80e6e11ad36009cd
Drake <dboul63@gmail.com>
2024-10-21 17:02:36 -0600

    Removed uneccesary device calls

--- ex3_obc_fsw/dev_tools/i2c_test/src/main.rs
+++ ex3_obc_fsw/dev_tools/i2c_test/src/main.rs
@@ -1,16 +1,18 @@
 use i2c::*;
 use std::{process::exit, thread::sleep, time};
 // Enter I2C bus path here
-const BUS_PATH: &str = "/dev/i2c-___";
+const BUS_PATH: &str = "/dev/i2c-0";
 
 // Enter Device Address here
 // Need to double check this as it depends on the device
 const ADDRESS: u16 = 0x48;
 
 //default config for ads1100
+#[allow(dead_code)]
 const CONFIG: u8 = 0x8C;
 
 // two bytes we need to send to device to do a general call reset if neccesary
+#[allow(dead_code)]
 const GENERAL_CALL_RESET: [u8; 2] = [0x00, 0x06];
 
 const SLEEP_TIME: time::Duration = time::Duration::from_millis(50);
@@ -18,32 +20,6 @@ const SLEEP_TIME: time::Duration = time::Duration::from_millis(50);
 fn main() {
     // Initialize the I2c interafce with lux meter
     let mut lux_meter = I2cDeviceInterface::new(BUS_PATH, ADDRESS).unwrap();
-
-    // Perform general call reset
-    match lux_meter.send(&GENERAL_CALL_RESET) {
-        Ok(val) => {
-            println!("Sent {}, bytes.", val);
-            println!("Successfully reset device.");
-        }
-        Err(e) => {
-            println!("Failed to send general call reset to device.");
-            println!("{}", e);
-        }
-    };
-    sleep(SLEEP_TIME);
-
-    // Write Confiuration Byte
-    // Do not need to set config since its already set at default, this code is here incase we want
-    // to change config from default
-    match lux_meter.send(&[CONFIG]) {
-        Ok(_) => {
-            println!("Successfully sent configuration byte.");
-        }
-        Err(e) => {
-            println!("Writing configuration to the ADS failed.");
-            println!("{}", e);
-        }
-    };
 
     sleep(SLEEP_TIME);
 
