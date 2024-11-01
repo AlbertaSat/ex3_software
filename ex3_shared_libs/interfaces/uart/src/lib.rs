4ca884287c4fa01b429b9399e0acc197b4fb9bb3
Drake <dboul63@gmail.com>
2024-10-31 20:23:27 -0600

    Changed UART interface to use system agnostic implementation of serialport

--- ex3_shared_libs/interfaces/uart/src/lib.rs
+++ ex3_shared_libs/interfaces/uart/src/lib.rs
@@ -1,39 +1,39 @@
 use message_structure::{deserialize_msg, serialize_msg, Msg};
-use serialport::TTYPort;
+use serialport::SerialPort;
 use std::io::{Error as IoError, Read, Write};
 
 // Interface to send and read Msg structures
 pub trait Interface {
-    fn read(&mut self) -> Result<Msg, IoError>;
-    fn send(&mut self, msg: &Msg) -> Result<usize, IoError>;
+    fn recv_msg(&mut self) -> Result<Msg, IoError>;
+    fn write_msg(&mut self, msg: &Msg) -> Result<usize, IoError>;
 }
 
 // Uart interface struct.
 pub struct UARTInterface {
-    interface: TTYPort,
+    interface: Box<dyn SerialPort>,
     device_name: String,
     baud_rate: u32,
 }
 
 impl Interface for UARTInterface {
     // reads a Msg structure from interface, returns a Msg struct.
-    fn read(&mut self) -> Result<Msg, IoError> {
+    fn recv_msg(&mut self) -> Result<Msg, IoError> {
         let mut bytes = Vec::new();
-        self.read_raw_bytes(&mut bytes)?;
+        self.recv(&mut bytes)?;
         deserialize_msg(&bytes)
     }
 
     // sends a Msg structure to interface, returns the bytes written.
-    fn send(&mut self, msg: &Msg) -> Result<usize, IoError> {
+    fn write_msg(&mut self, msg: &Msg) -> Result<usize, IoError> {
         let bytes = serialize_msg(msg)?;
-        self.send_raw_bytes(&bytes)
+        self.write(&bytes)
     }
 }
 
 impl UARTInterface {
     // Constructor for UARTInterface.
     pub fn new(device_name: &str, baud_rate: u32) -> Result<UARTInterface, IoError> {
-        let interface = TTYPort::open(&serialport::new(device_name, baud_rate))?;
+        let interface = serialport::new(device_name, baud_rate).open()?;
         Ok(UARTInterface {
             interface,
             device_name: device_name.to_string(),
@@ -42,12 +42,12 @@ impl UARTInterface {
     }
 
     // reads raw bytes from uart interface into buffer, returns the number of bytes read.
-    pub fn read_raw_bytes(&mut self, buffer: &mut [u8]) -> Result<usize, IoError> {
+    pub fn recv(&mut self, buffer: &mut [u8]) -> Result<usize, IoError> {
         self.interface.read(buffer)
     }
 
     // sends raw bytes in buffer to uart interface, returns the number of bytes written.
-    pub fn send_raw_bytes(&mut self, buffer: &[u8]) -> Result<usize, IoError> {
+    pub fn write(&mut self, buffer: &[u8]) -> Result<usize, IoError> {
         self.interface.write(buffer)
     }
 
