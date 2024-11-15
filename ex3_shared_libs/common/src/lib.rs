935cb16e530f091cb19fa92b942c11ad1bb494ec
Drake <dboul63@gmail.com>
2024-11-15 10:53:44 -0700

    Made simulated UHF broadcast and UART port names more descriptive

--- ex3_shared_libs/common/src/lib.rs
+++ ex3_shared_libs/common/src/lib.rs
@@ -3,9 +3,9 @@ pub mod ports {
     pub const SIM_DFGM_PORT: u16 = 1802;
     pub const SIM_ADCS_PORT: u16 = 1803;
     pub const SIM_EPS_PORT: u16 = 1804;
-    pub const SIM_COMMS_PORT: u16 = 1805;
+    pub const SIM_ESAT_UART_PORT: u16 = 1805;
     pub const SIM_IRIS_PORT: u16 = 1806;
-    pub const SIM_UHF_GS_PORT: u16 = 1808;
+    pub const SIM_ESAT_UHF_PORT: u16 = 1808;
 
     pub const DFGM_HANDLER_DISPATCHER_PORT: u16 = 1900;
     pub const SCHEDULER_DISPATCHER_PORT: u16 = 1901;
