f5b3e6cd30ddeb5865872d6079280238544b0868
leander <lflopez@ualberta.ca>
2025-01-11 07:52:13 +0000

    started navigation bar for subsystems

---
+++ ex3_ground_station/gui_ground_station/src/main.rs
@@ -0,0 +1,18 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <ul>
+            <li><a href="default.asp">{"Bulk Message Dispatcher"}</a></li>
+            <li><a href="news.asp">{"UHF Subsystem (simulated)"}</a></li>
+            <li><a href="contact.asp">{"Comms Handler"}</a></li>
+            <li><a href="about.asp">{"Command Dispatcher"}</a></li>
+            <li><a href="about.asp">{"Ground Station"}</a></li>
+        </ul>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
